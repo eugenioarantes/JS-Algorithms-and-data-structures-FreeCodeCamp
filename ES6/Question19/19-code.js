cdfe7e20046b10575302e7f4b027042f08fa90d2
Eugenio <eugeniolopesarantesjunior@gmail.com>
2023-03-01 08:41:05 -0300

    Adding ES6 question 19

---
+++ ES6/Question19/19-code.js
@@ -0,0 +1,8 @@
+// Only change code below this line
+
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
